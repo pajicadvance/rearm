c94c8d4fbcd9c85b41b0e0ac9df236a714ab3716
pajicadvance <pavle.pajic23@gmail.com>
2024-10-26 20:18:03 +0200

    Custom heart rendering for Bleeding (broken atm)

---
+++ src/main/java/me/pajic/rearm/EarlyRiser.java
@@ -0,0 +1,34 @@
+package me.pajic.rearm;
+
+import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Supplier;
+
+public class EarlyRiser implements Runnable {
+
+    private static final String targetType =
+            FabricLoader.getInstance().getMappingResolver().mapClassName("intermediary", "net.minecraft.class_329$class_6411");
+
+    private static final String paramType =
+            "L" + FabricLoader.getInstance().getMappingResolver().mapClassName("intermediary", "net.minecraft.class_2960");
+
+    @Override
+    public void run() {
+        ClassTinkerers.enumBuilder(targetType, paramType, paramType, paramType, paramType, paramType, paramType, paramType, paramType)
+                .addEnum("BLEEDING", () -> {
+                    Supplier<Supplier<Object[]>> supplier = (() -> () -> new Object[] {
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_full"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_full_blinking"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_half"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_half_blinking"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_hardcore_full"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_hardcore_full_blinking"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_hardcore_half"),
+                            ResourceLocation.fromNamespaceAndPath("rearm", "hud/heart/bleeding_hardcore_half_blinking")
+                    });
+                    return supplier.get().get();
+                }).build();
+    }
+}

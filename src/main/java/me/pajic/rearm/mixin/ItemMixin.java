0bae34bf7867a9361b9acaf08ce24fc7b48db06a
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-05-06 03:44:27 +0200

    Hold My Items compat

--- src/main/java/me/pajic/rearm/mixin/ItemMixin.java
+++ src/main/java/me/pajic/rearm/mixin/ItemMixin.java
@@ -3,7 +3,6 @@ package me.pajic.rearm.mixin;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import me.pajic.rearm.ability.CripplingThrowAbility;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
@@ -13,6 +12,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import net.neoforged.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -32,7 +32,7 @@ public abstract class ItemMixin {
     @WrapMethod(method = "getUseAnimation")
     private UseAnim axe_useAnimaton(ItemStack stack, Operation<UseAnim> original) {
         if (stack.is(ItemTags.AXES)) {
-            if (FabricLoader.getInstance().isModLoaded("hold-my-items")) return UseAnim.NONE;
+            if (ModList.get().isLoaded("holdmyitems")) return UseAnim.NONE;
             return UseAnim.SPEAR;
         }
         return original.call(stack);

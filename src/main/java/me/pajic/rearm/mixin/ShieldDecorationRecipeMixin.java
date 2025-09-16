645cedc267754a61e6572d71ab6f6fea5a6d9e93
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-09-16 14:31:36 +0200

    More robust mixin

--- src/main/java/me/pajic/rearm/mixin/ShieldDecorationRecipeMixin.java
+++ src/main/java/me/pajic/rearm/mixin/ShieldDecorationRecipeMixin.java
@@ -1,8 +1,9 @@
 package me.pajic.rearm.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.pajic.rearm.item.ReArmItems;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.ShieldDecorationRecipe;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,25 +12,25 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(ShieldDecorationRecipe.class)
 public class ShieldDecorationRecipeMixin {
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "matches(Lnet/minecraft/world/item/crafting/CraftingInput;Lnet/minecraft/world/level/Level;)Z",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"
             )
     )
-    private boolean matches_extendShieldCheck(boolean original, @Local(name = /*? if 1.21.1 {*/"itemStack3"/*?}*//*? if >= 1.21.7 {*//*"itemStack"*//*?}*/) ItemStack itemStack3) {
-        return original || itemStack3.is(ReArmItems.NETHERITE_SHIELD);
+    private boolean matches_extendShieldCheck(ItemStack instance, Item item, Operation<Boolean> original) {
+        return original.call(instance, item) || instance.is(ReArmItems.NETHERITE_SHIELD);
     }
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "assemble(Lnet/minecraft/world/item/crafting/CraftingInput;Lnet/minecraft/core/HolderLookup$Provider;)Lnet/minecraft/world/item/ItemStack;",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"
             )
     )
-    private boolean assemble_extendShieldCheck(boolean original, @Local(name = "itemStack3") ItemStack itemStack3) {
-        return original || itemStack3.is(ReArmItems.NETHERITE_SHIELD);
+    private boolean assemble_extendShieldCheck(ItemStack instance, Item item, Operation<Boolean> original) {
+        return original.call(instance, item) || instance.is(ReArmItems.NETHERITE_SHIELD);
     }
 }

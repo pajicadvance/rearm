c872aca0ba69b682b6bd2dea72c1602a206702f2
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-10-30 14:36:37 +0100

    2.4.5

--- src/main/java/me/pajic/rearm/mixin/ArmorMaterialMixin.java
+++ src/main/java/me/pajic/rearm/mixin/ArmorMaterialMixin.java
@@ -1,11 +1,9 @@
 package me.pajic.rearm.mixin;
 
-import com.google.common.collect.Maps;
 import com.llamalad7.mixinextras.sugar.Local;
 import me.pajic.rearm.Main;
-import me.pajic.rearm.config.ArmorMaterialHelper;
+import me.pajic.rearm.util.ArmorMaterialHelper;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -24,7 +22,6 @@ import net.minecraft.world.item.equipment.ArmorType;
 import net.minecraft.world.item.equipment.EquipmentAsset;
 *///?}
 
-import java.util.Arrays;
 import java.util.Map;
 
 @Mixin(ArmorMaterial.class)
@@ -59,39 +56,10 @@ public class ArmorMaterialMixin {
                         targetTotal += (int) (entry.getValue() * Main.CONFIG.armor.armorMultiplier.get());
                 }
             }
-            int[] values = {
-                    Math.round(targetTotal * ((float) Main.CONFIG.armor.chestplateArmorPercent.get() / 100)),
-                    Math.round(targetTotal * ((float) Main.CONFIG.armor.leggingsArmorPercent.get() / 100)),
-                    Math.round(targetTotal * ((float) Main.CONFIG.armor.helmetArmorPercent.get() / 100)),
-                    Math.round(targetTotal * ((float) Main.CONFIG.armor.bootsArmorPercent.get() / 100))
-            };
-            int body = Math.round(targetTotal * ((float) Main.CONFIG.armor.bodyArmorPercent.get() / 100));
-            int total = Arrays.stream(values).sum();
-            if (total != targetTotal) {
-                int i = 0;
-                while (total != targetTotal) {
-                    if (total > targetTotal) {
-                        values[i]--;
-                        total--;
-                    } else {
-                        values[i]++;
-                        total++;
-                    }
-                    i = i == 3 ? 0 : i + 1;
-                }
-            }
-            defense = Maps.newEnumMap(Map.of(
-                    /*? if 1.21.1 {*/ArmorItem.Type/*?} else {*//*ArmorType*//*?}*/.HELMET, values[2],
-                    /*? if 1.21.1 {*/ArmorItem.Type/*?} else {*//*ArmorType*//*?}*/.CHESTPLATE, values[0],
-                    /*? if 1.21.1 {*/ArmorItem.Type/*?} else {*//*ArmorType*//*?}*/.LEGGINGS, values[1],
-                    /*? if 1.21.1 {*/ArmorItem.Type/*?} else {*//*ArmorType*//*?}*/.BOOTS, values[3],
-                    /*? if 1.21.1 {*/ArmorItem.Type/*?} else {*//*ArmorType*//*?}*/.BODY, body
-            ));
-            toughness = 0;
+            if (targetTotal > 0) defense = ArmorMaterialHelper.calculateDefenses(targetTotal);
+            if (Main.CONFIG.armor.enchantmentBasedToughness.get()) toughness = 0;
             if (Main.CONFIG.armor.defenseBasedKnockbackResist.get()) {
-                knockbackResistance = Mth.lerp(
-                        targetTotal / (20 * Main.CONFIG.armor.armorMultiplier.get()), 0, 0.1F
-                );
+                knockbackResistance = ArmorMaterialHelper.calculateKnockbackResist(targetTotal);
             }
         }
     }
